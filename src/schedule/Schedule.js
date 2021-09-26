f86553f4c9306da464fa1eae46d7a8da6f5dfc7e
Chris Vettese <chris.vettese2@gmail.com>
2021-09-26 02:40:01 -0400

    Improved timeline on mobile

--- src/schedule/Schedule.js
+++ src/schedule/Schedule.js
@@ -12,12 +12,20 @@ export default function Schedule() {
         <VerticalTimeline>
           {
             events.map((event, index) => {
-              const borderWidth = index % 2 === 0 ? '0 0 0 6px' : '0 6px 0 0';
+              let borderWidth;
+              //Magic number for when to make all timeline elements appear on same side
+              const switchingTimeline = document.body.getBoundingClientRect().width > 1152;
+              if (switchingTimeline) {
+                borderWidth = index % 2 === 0 ? '0 0 0 6px' : '0 6px 0 0';
+              } else {
+                borderWidth = '0 6px 0 0';
+              }
               return (
                 <VerticalTimelineElement key={index}
                                          contentStyle={{background: 'rgb(230, 230, 230)', color: 'black', border: `solid ${event.iconColor}`, borderWidth: borderWidth, borderRadius: '3px', boxShadow: 'none'}}
                                          contentArrowStyle={{borderRight: '7px solid  rgb(230, 230, 230)'}}
                                          date={event.time}
+                                         position={switchingTimeline ? (index % 2 === 0 ? 'left' : 'right') : 'right'}
                                          dateClassName={'TimelineDate'}
                                          iconStyle={{background: event.iconColor, color: '#fff', boxShadow: 'none'}}
                                          icon={event.icon}

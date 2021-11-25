ecbdfdc31d3d31994bd160640215aba26107911f
Chris Vettese <chris.vettese2@gmail.com>
2021-11-25 14:51:46 -0500

    Update deploy script

--- scripts/Deploy.sh
+++ scripts/Deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 npm run deploy;
-git fetch;
 BRANCH=$(git rev-parse --abbrev-ref HEAD);
 git checkout main;
 git branch -D gh-pages;
 git branch -D prod;
-git checkout origin/gh-pages;
+git fetch;
+git checkout gh-pages;
 echo 'ieeespac.ca' > ../CNAME;
 git checkout -b prod;
 git add -A;
